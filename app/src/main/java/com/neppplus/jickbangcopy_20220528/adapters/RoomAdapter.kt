69320d21144ee64b1b4c2a0d51038f1244674ae1
조경진 <cho881020@gmail.com>
2022-06-04 10:20:36 +0900

    어댑터 클래스 - getView 함수 기초 작업

--- app/src/main/java/com/neppplus/jickbangcopy_20220528/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220528/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.jickbangcopy_20220528.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20220528.R
 import com.neppplus.jickbangcopy_20220528.datas.RoomData
 
 class RoomAdapter(
@@ -9,4 +13,19 @@ class RoomAdapter(
     val resId: Int,
     val mList: ArrayList<RoomData>
 ) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+
+            tempRow =  LayoutInflater.from(mContext).inflate( R.layout.room_list_item, null )
+
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
